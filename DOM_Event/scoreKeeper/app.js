af44b720477797d8a053b2703ddf77c5dc088790
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-04-19 10:44:44 -0700

    Style added. 'Deuce' feature added

--- DOM_Event/scoreKeeper/app.js
+++ DOM_Event/scoreKeeper/app.js
@@ -1,35 +1,45 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display'),
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display'),
+}
+
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#winningScore');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
 let p2Score = 0;
-
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function () {
+function updateScores(player, opponent) {
     if (!isGameOver) {
-        p1Score += 1;
-        p1Display.textContent = p1Score;
-        if (p1Score == winningScore) {
+        player.score += 1;
+        player.display.textContent = player.score;
+
+        if ((player.score >= winningScore) && (player.score - opponent.score >= 2)) {
             isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
         }
     }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
 });
 
 
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) {
-        p2Score += 1;
-        p2Display.textContent = p2Score;
-        if (p2Score == winningScore) {
-            isGameOver = true;
-        }
-    }
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
 });
 
 winningScoreSelect.addEventListener('change', function () {
@@ -37,13 +47,15 @@ winningScoreSelect.addEventListener('change', function () {
     reset();
 });
 
-
 resetButton.addEventListener('click', reset);
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
+
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 }
